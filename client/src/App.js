135e3ae68a15e393b3c222b3731bd7b03fa43c1a
Erik De Luna <codingerik1.gmail.com>
2020-09-10 17:26:33 -0500

    fixing proxy bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,28 @@
 import React from "react";
-import Search from "./pages/Search";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Books from "./pages/Books";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
-import Saved from "./pages/Saved"; 
 import Nav from "./components/Nav";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-// The app will not render correctly until you setup a Route component.
-// Refer to the Basic Example documentation if you need to.
-// (https://reacttraining.com/react-router/web/example/basic)
 function App() {
   return (
-    <BrowserRouter>
-    <div>
-      <Nav/>
+    <Router>
+      <div>
+        <Nav />
         <Switch>
-        <Route exact path="/" component={Search} />
-        <Route exact path="/books" component={Search} />
-        <Route exact path="/books/:id" component={Detail} />
-        <Route exact path="/saved" component={Saved} />
-        <Route exact path="/*" component={NoMatch} />
+          <Route exact path={["/", "/books"]}>
+            <Books />
+          </Route>
+          <Route exact path="/books/:id">
+            <Detail />
+          </Route>
+          <Route>
+            <NoMatch />
+          </Route>
         </Switch>
-    </div>
-    </BrowserRouter>
+      </div>
+    </Router>
   );
 }
 
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,21 +1,11 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
 
 function Nav() {
-
-  const location = useLocation();
-
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-info">
-      <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
-        Google Books
-        </Link>
-      <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
-        Search
-        </Link>
-      <Link to="/saved" className={location.pathname === "/saved" ? "nav-link active" : "nav-link"}>
-        Saved
-        </Link>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a className="navbar-brand" href="/">
+        React Reading List
+      </a>
     </nav>
   );
 }
