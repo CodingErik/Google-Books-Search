7958db4828469b796849737c7ad633b0c167cdbc
Erik De Luna <codingerik1.gmail.com>
2020-09-11 15:03:53 -0500

    test4

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -21,7 +21,7 @@ import axios from "axios";
 
 export default {
   searchTitle: function(query) {
-    return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}&key=${SECRET_KEY}`)
+    return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}&key=${process.env.SECRET_KEY}`)
   }
 }
 
