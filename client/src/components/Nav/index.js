3858137da0f36f84962b00d8f29abbd4adea115c
Erik De Luna <codingerik1.gmail.com>
2020-09-11 11:01:41 -0500

    data is pulling from the database

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Navbar from 'react-bootstrap/Navbar'
 
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <Link className="navbar-brand" to="/">
         React Reading List
-      </Link>
+</Link>
       <Link className="navbar-brand" to="/">
         Pupster
-      </Link>
+</Link>
     </nav>
   );
 }
 
 export default Nav;
+
+
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -60,6 +60,7 @@ function Search() {
                 <Col size="md-6">
                     <Jumbotron>
                         <h1>(React) Google Books Search</h1>
+                        <h3>Search for and Save Books of Interest</h3>
                     </Jumbotron>
                     <form>
                         <Input
