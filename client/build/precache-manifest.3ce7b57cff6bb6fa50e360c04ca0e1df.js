c90a948d60d66384c3feea3340e0f49e69d068f3
Erik De Luna <codingerik1.gmail.com>
2020-09-15 16:26:05 -0500

    deploying the build folder

---
+++ client/build/precache-manifest.3ce7b57cff6bb6fa50e360c04ca0e1df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8c68052363887d7e4c9",
+    "url": "/static/css/main.ef5db3ff.chunk.css"
+  },
+  {
+    "revision": "a8c68052363887d7e4c9",
+    "url": "/static/js/main.c3698735.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "09154e14ccb5759f8893",
+    "url": "/static/css/2.7832d072.chunk.css"
+  },
+  {
+    "revision": "09154e14ccb5759f8893",
+    "url": "/static/js/2.eff563d3.chunk.js"
+  },
+  {
+    "revision": "71f40fb14cd302792646da6ff06b1374",
+    "url": "/index.html"
+  }
+];
